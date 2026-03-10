5d1f7a204b11ea1228861bd98a5d8e325aa787bc
Ains1337 <patrick.fanroth@gmail.com>
2026-03-10 16:21:19 +0100

    update text-white darkmodeTogglePlan and centralize RoutesPlan

--- frontend/src/pages/player-or-dungeon-master.tsx
+++ frontend/src/pages/player-or-dungeon-master.tsx
@@ -4,7 +4,7 @@ export function PlayerOrDungeonMaster() {
   return (
     <>
       <h1>Decide Player or Game-Master</h1>
-      <A href="/secure/player">Player</A>
+      <A class="inline-flex flex-1 justify-center rounded-xl border px-6 py-4 text-lg font-semibold"href="/secure/player">Player</A>
       <A href="/secure/dungeon-master">Dungeon-Master</A>
     </>
   );
